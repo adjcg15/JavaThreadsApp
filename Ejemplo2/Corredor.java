61d44a77c5b4c633702c094b1308102ca705972c
Angel Garcia <adjcg15@gmail.com>
2024-03-12 10:12:22 -0600

    initial commit

---
+++ Ejemplo2/Corredor.java
@@ -0,0 +1,30 @@
+package Ejemplo2;
+
+public class Corredor implements Runnable {
+    private String avance;
+    private long latencia;
+    private char c;
+
+    public Corredor(char c) {
+        this.avance = "";
+        this.latencia = (long)(Math.random() * 100);
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 70; i++) {
+            avance = avance + c;
+            System.out.println(avance);
+
+            try {
+                Thread.sleep(latencia);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        avance += " terminé!!!";
+        System.out.println(avance);
+    }
+}
